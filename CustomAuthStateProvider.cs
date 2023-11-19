6563f60d5428bebf880a7a370d83a2a8e0cb08fc
paul1610 <nellpaul78@gmail.com>
2023-11-19 16:03:43 +0100

    Auth from server

--- CustomAuthStateProvider.cs
+++ CustomAuthStateProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
+using ConfigurationTool.Utils;
 
 namespace ConfigurationTool;
 
@@ -9,10 +10,15 @@ public class CustomAuthStateProvider : AuthenticationStateProvider
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
         // TODO: Get token from backend
-        string token =
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6InRlc3QiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIyIiwiZXhwIjoxNzAwNDAxNTYyLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUyNjAiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjUyNjAifQ.QfgwOimLhG8qVTJ-BXcZ1ZOUJPwT-ii8WOkDVPpOkn0";
+        string? token = await HttpUtils.Post<string>("authentication", new  { UserIdentificator = "test", Password = "test" });
+
+        if (token == null)
+        {
+            // TODO: handle
+        }
+
         AuthenticationState state;
-        var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+        var identity = new ClaimsIdentity(AuthenticationUtils.ParseClaimsFromJwt(token), "jwt");
         // var identity = new ClaimsIdentity();
         var user = new ClaimsPrincipal(identity);
         state = new AuthenticationState(user);
@@ -21,21 +27,5 @@ public class CustomAuthStateProvider : AuthenticationStateProvider
 
         return state;
     }
-    public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
-    {
-        var payload = jwt.Split('.')[1];
-        var jsonBytes = ParseBase64WithoutPadding(payload);
-        var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
-        return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
-    }
 
-    private static byte[] ParseBase64WithoutPadding(string base64)
-    {
-        switch (base64.Length % 4)
-        {
-            case 2: base64 += "=="; break;
-            case 3: base64 += "="; break;
-        }
-        return Convert.FromBase64String(base64);
-    }
 }
