93b7eb3490eb6ca599458720abd2b4c7f7ddb49f
Jonas Pamminger <j.pamminger@HTBLALeonding.onmicrosoft.com>
2023-12-01 09:39:46 +0100

    add missing changes

--- Utils/HttpUtils.cs
+++ Utils/HttpUtils.cs
@@ -1,42 +1,20 @@
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 
 namespace ConfigurationTool.Utils;
 
-public static class HttpUtils
+public class HttpUtils
 {
     private const string BASE_URL = "http://localhost:5260/";
-    
-    public static async Task<T?> Get<T>(string url)
+
+    public static async Task<T?> Get<T>(string url, string jwtToken = "")
     {
         try
         {
             var request = new HttpRequestMessage(HttpMethod.Get, BASE_URL + url);
             var client = new HttpClient();
-            var response = await client.SendAsync(request);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                return JsonSerializer.Deserialize<T>(content,
-                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            }
-        }
-        catch
-        {
-            // ignored
-        }
-
-        return default;
-    }
-
-    public static async Task<T?> Post<T>(string url, object obj)
-    {
-        try
-        {
-            var request = new HttpRequestMessage(HttpMethod.Post, BASE_URL + url);
-            request.Content = new StringContent(JsonSerializer.Serialize(obj), Encoding.UTF8, "application/json");
-            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
             var response = await client.SendAsync(request);
 
             if (response.IsSuccessStatusCode)
@@ -58,18 +36,23 @@ public static class HttpUtils
         return default;
     }
 
-    public static async Task<T?> Put<T>(string url, object obj)
+    public static async Task<T?> Post<T>(string url, object obj, string jwtToken = "")
     {
         try
         {
-            var request = new HttpRequestMessage(HttpMethod.Put, BASE_URL + url);
+            var request = new HttpRequestMessage(HttpMethod.Post, BASE_URL + url);
             request.Content = new StringContent(JsonSerializer.Serialize(obj), Encoding.UTF8, "application/json");
             var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
             var response = await client.SendAsync(request);
 
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
+
+                if (typeof(T) == typeof(string))
+                    return (T)(object)content;
+
                 return JsonSerializer.Deserialize<T>(content,
                     new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             }
@@ -82,17 +65,51 @@ public static class HttpUtils
         return default;
     }
 
-    public static async Task<T?> Delete<T>(string url)
+    public static async Task<T?> Put<T>(string url, object obj, string jwtToken = "")
     {
         try
         {
-            var request = new HttpRequestMessage(HttpMethod.Delete, BASE_URL + url);
+            var request = new HttpRequestMessage(HttpMethod.Put, BASE_URL + url);
+            request.Content = new StringContent(JsonSerializer.Serialize(obj), Encoding.UTF8, "application/json");
             var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
             var response = await client.SendAsync(request);
 
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
+
+                if (typeof(T) == typeof(string))
+                    return (T)(object)content;
+
+                return JsonSerializer.Deserialize<T>(content,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+        }
+        catch
+        {
+            // ignored
+        }
+
+        return default;
+    }
+
+    public static async Task<T?> Delete<T>(string url, string jwtToken = "")
+    {
+        try
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, BASE_URL + url);
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                if (typeof(T) == typeof(string))
+                    return (T)(object)content;
+
                 return JsonSerializer.Deserialize<T>(content,
                     new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             }
