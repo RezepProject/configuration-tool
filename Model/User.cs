d10a32590456d21311bc1f0f7f13ebf310a102cf
paul1610 <nellpaul78@gmail.com>
2023-12-02 16:28:10 +0100

    fix list

--- Model/User.cs
+++ Model/User.cs
@@ -14,3 +14,12 @@ public class CreateUser
     public string Email { get; set; }
     public int RoleId { get; set; }
 }
+
+public class Invitation
+{
+    public int Id { get; set; }
+    public Guid Token { get; set; }
+    public string Email { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int RoleId { get; set; }
+}
