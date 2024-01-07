9b9f88139b6387cb5691d96b4f7ca56673786338
Jonas Pamminger <j.pamminger@HTBLALeonding.onmicrosoft.com>
2024-01-07 16:35:14 +0100

    nothing works

--- Model/Answer.cs
+++ Model/Answer.cs
@@ -5,8 +5,6 @@ public class Answer
     public int Id { get; set; }
     public string Text { get; set; }
     public string User { get; set; }
-    public int QuestionId { get; set; }
-    public Question? Question { get; set; }
     public bool IsEditing { get; set; } = false;
 }
 
@@ -20,5 +18,4 @@ public class UpdateAnswer
 {
     public string Text { get; set; }
     public string User { get; set; }
-    public int? QuestionId { get; set; }
 }
