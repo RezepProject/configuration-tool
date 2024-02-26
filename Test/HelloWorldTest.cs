608ba20103a375bd03025a811ac852f2e5358ed2
Fabian Baitura <peacemakerdude3@gmail.com>
2024-02-26 12:00:58 +0100

    adding bUnit dependencies

---
+++ Test/HelloWorldTest.cs
@@ -0,0 +1,23 @@
+﻿using Bunit;
+using Xunit;
+
+namespace ConfigurationTool.Test;
+
+public class HelloWorldTest
+{
+    [Fact]
+    public void diff()
+    {
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public void ThisIsATest()
+    {
+        using var ctx = new TestContext();
+        var cut = ctx.RenderComponent<HelloWorld>();
+        
+        cut.MarkupMatches("@<h1>Hello world from Blazor</h1>");
+        Assert.True(true);
+    }
+}
