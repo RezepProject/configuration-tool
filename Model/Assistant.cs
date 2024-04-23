3cbc9b2cf3ecb7d6e75828e18c5a4862bb4702bf
Leon_Leeb <you.t5be@gmx.at>
2024-04-23 10:01:03 +0200

    started config for peppers

---
+++ Model/Assistant.cs
@@ -0,0 +1,11 @@
+﻿namespace ConfigurationTool.Model;
+
+public class Assistant
+{
+    public string Name { get; set; }
+    public string BackgroundImageUrl { get; set; }
+    public string Language { get; set; }
+    public double TalkingSpeed { get; set; } = 1;
+    public string GreetingMessage { get; set; }
+    public bool IsExpanded { get; set; } = true;
+}
