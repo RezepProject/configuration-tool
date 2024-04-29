aa01ec0818f909ce76a1fe88267c5049d196d5af
Leon_Leeb <you.t5be@gmx.at>
2024-04-29 21:34:25 +0200

    loding in the assistants from backend

--- Model/Assistant.cs
+++ Model/Assistant.cs
@@ -2,10 +2,14 @@
 
 public class Assistant
 {
+    public int Id { get; set; }
+    public int ConfigUserId { get; set; }
+    public int ConfigUser { get; set; }
     public string Name { get; set; }
-    public string BackgroundImageUrl { get; set; }
+    public string BackgroundImage { get; set; }
     public string Language { get; set; }
-    public double TalkingSpeed { get; set; } = 1;
+    public double TalkingSpeed { get; set; }
     public string GreetingMessage { get; set; }
-    public bool IsExpanded { get; set; } = true;
+    public bool State { get; set; }
+    //public bool IsExpanded { get; set; }
 }
