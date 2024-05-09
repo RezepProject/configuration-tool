14a05a1c503990cde8b2a74b83b3cc0f4d420b12
Jonas Pamminger <j.pamminger@HTBLALeonding.onmicrosoft.com>
2024-05-09 17:38:39 +0200

    fixed valiation

--- Model/Answer.cs
+++ Model/Answer.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FluentValidation;
 
 namespace ConfigurationTool.Model;
 
@@ -16,4 +16,23 @@ public class UpdateAnswer
     public string? Text { get; set; }
     public string? User { get; set; }
     public int? QuestionId { get; set; }
+}
+
+public class CreateAnswer
+{
+    public string? Text { get; set; }
+
+    public string? User { get; set; }
+}
+
+public class CreateAnswerValidator : ValidatorBase<CreateAnswer>
+{
+    public CreateAnswerValidator()
+    {
+        RuleFor(a => a.Text)
+            .MinimumLength(10)
+            .WithMessage("min length 10")
+            .NotNull()
+            .NotEmpty();
+    }
 }
--- Model/Question.cs
+++ Model/Question.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+
 
 namespace ConfigurationTool.Model;
 
@@ -8,4 +10,35 @@ public class Question
     public string Text { get; set; } = String.Empty;
 
     public List<Answer> Answers { get; set; } = new();
+}
+
+public class CreateQuestion
+{
+    public string Text { get; set; } = String.Empty;
+    public List<CreateAnswer> Answers { get; set; } = new();
+}
+
+public class AnswerListValidator : AbstractValidator<List<CreateAnswer>>
+{
+    public AnswerListValidator()
+    {
+        RuleForEach(a => a)
+            .SetValidator(new CreateAnswerValidator());
+    }
+}
+
+public class CreateQuestionValidator : ValidatorBase<CreateQuestion>
+{
+    public CreateQuestionValidator()
+    {
+        RuleFor(q => q.Text)
+            .MinimumLength(10)
+            .WithMessage("min length 10")
+            .NotNull()
+            .NotEmpty();
+        RuleFor(q => q.Answers)
+            .NotNull()
+            .SetValidator(new AnswerListValidator())
+            .NotEmpty();
+    }
 }
