e63a942d6644282eb0e0e66783e785bf58a44538
Jonas Pamminger <j.pamminger@HTBLALeonding.onmicrosoft.com>
2024-05-20 15:30:40 +0200

    started fixing refesh token

--- Model/UserLoginDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConfigurationTool.Model;
-
-public class UserLoginDto
-{
-    public string UserIdentificator { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
