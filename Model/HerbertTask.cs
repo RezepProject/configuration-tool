5625536f475485ce2fa4c17b4f09e87ab6bdd848
Jonas Pamminger <jonas.pamminger@outlook.de>
2025-01-23 18:41:53 +0100

    task tabelle

---
+++ Model/HerbertTask.cs
@@ -0,0 +1,20 @@
+﻿namespace ConfigurationTool.Model;
+
+public class HerbertTask
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public bool Done { get; set; }
+}
+
+public class CreateTask
+{
+    public string Text { get; set; }
+    public bool Done { get; set; }
+}
+
+public class UpdateTask
+{
+    public string Text { get; set; }
+    public bool Done { get; set; }
+}
